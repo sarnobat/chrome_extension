11d7248f8b6169bc72d2457fa14185c838a13aa5
Sridhar Sarnobat <ss401533@gmail.com>
2016-08-07 17:11:10 -0700

    Minor changes (untested)

--- yurl/popup.js
+++ yurl/popup.js
@@ -1,10 +1,15 @@
+// Find out the ID of the current tab
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	var tab = tabs[0];
 	
 	// TODO: Go immediately to background script. This popup's thread of execution will get killed as soon as we move to another tab.
 	
+	
+	// invoke the stash operation
 	chrome.tabs.update(tab.id,
 		{url: 'http://netgear.rohidekar.com/yurl/stash2.html?url=' + encodeURIComponent(tab.url) + '&nodeId=45' },
+		
+		// Do all the closing related logic
 		function(){
 			// Wait 5 seconds. It seems like sending it immediately doesn't give a chance for the content script to load
 			setTimeout(function () {
