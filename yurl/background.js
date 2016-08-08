544265e83342bfa456b803ec256808d6fdebc74a
Sridhar Sarnobat <ss401533@gmail.com>
2016-08-07 17:44:05 -0700

    Minor changes (untested)

--- yurl/background.js
+++ yurl/background.js
@@ -1,48 +1,35 @@
 
 chrome.extension.onRequest.addListener(function(request, sender)
 {
-
-
 	if (request.message == "popup requested a close") {
 
 		var tab = request.tab;	
 		chrome.tabs.update(request.tab.id,
 			{url: 'http://netgear.rohidekar.com/yurl/stash2.html?url=' + encodeURIComponent(tab.url) + '&nodeId=45' },
-	
+
 			// Do all the closing related logic
 			function(){
-
 				repeatedlyAttemptClose(tab);
 			});		
 	} else if (request.message = "inner HTML starts with success? true") {
 		// This will never get called, delete this case
-	
-		console.debug("DON'T DO HERE: GotresponseGot response from background script");	
-		
-		//chrome.tabs.remove(request.tab.id, function (){});
-		//alert(JSON.stringify(request) );
+		console.debug("This will never get called, delete this case");	
 	}
-
 });
 
 function repeatedlyAttemptClose(tab){
 
 	// Wait 5 seconds. It seems like sending it immediately doesn't give a chance for the content script to load
 	setTimeout(function () {
-		console.debug("Checking if stashing was successful....");
 		// Check if stash was successful
 		chrome.tabs.sendMessage(tab.id, {tab: tab.id, counter: 0, message : "was stashing tab " + tab.id +  " successful?" }, null, function handler(response) {
-			console.debug("Received response");
 			console.debug(response.message);
 			if (response.message == 'found success') {
-				console.debug('Able to close tab ' + tab.id);
 				chrome.tabs.remove(tab.id, function (){});
 			} else {
-				//console.debug('Cannot close');
+				console.debug('Cannot close yet, will try again in 5 seconds');
 				repeatedlyAttemptClose(tab);
 			}
 		});
 	}, 5000);
-
-
 }
--- yurl/content.js
+++ yurl/content.js
@@ -1,30 +1,19 @@
-// Copyright (c) 2013 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-/*
-chrome.runtime.onConnect.addListener(function(port) {
-  port.onMessage.addListener(function(msg) {
-	console.debug('chrome.runtime.onConnect.addListener');
-    sendResponse({counter: request.counter+1});
-//    port.postMessage({counter: msg.counter+1});
-  });
-});
-*/
 chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  
-  	console.debug('1 chrome.extension.onRequest.addListener: ' + request);
-	var success =   document.getElementsByTagName("body")[0].innerHTML.startsWith("Success");
-	console.debug('inner HTML = ' + success);
-	if (success) {
-		// send to background script
-//		chrome.extension.sendRequest({tab: request.tab, message: "inner HTML starts with success? " + success});
-		
-		// send to popup
-		sendResponse({tab: request.tab, counter: request.counter+1, message : "found success" });
-	} else {
-
+	function(request, sender, sendResponse) {
+		var success =  document.getElementsByTagName("body")[0].innerHTML.startsWith("Success");
+		if (success) {		
+			// respond to background script telling it to close the tab
+			sendResponse({
+				tab: request.tab,
+				counter: request.counter+1,
+				message : "found success" 
+			});
+		} else {
+			sendResponse({
+				tab: request.tab,
+				counter: request.counter+1,
+				message : "did not yet succeed" 
+			});
+		}
 	}
-  });
-
-var n = 0;
+);
