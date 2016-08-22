f3aeddea8ab799acc982867ee83060d1602ec4bb
Sridhar Sarnobat <ss401533@gmail.com>
2016-08-21 20:16:40 -0700

    Minor changes (untested)

--- yurl/background.js
+++ yurl/background.js
@@ -3,8 +3,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		console.debug('button clicked: ' + tabs[0]);
 		
-		
-		
 		var tab = tabs[0];	
 		
 		if (tab.url.startsWith('http://netgear')) {
@@ -27,10 +25,27 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
 });
 
-chrome.extension.onRequest.addListener(function(request, sender)
-{
+function repeatedlyAttemptClose(tab){
+	console.debug('repeatedlyAttemptClose() begin');
+	// Wait 5 seconds. It seems like sending it immediately doesn't give a chance for the content script to load
+	setTimeout(function () {
+		// Check if stash was successful
+		chrome.tabs.sendMessage(tab.id, {tab: tab.id, counter: 0, message : "was stashing tab " + tab.id +  " successful?" }, null, function handler(response) {
+			console.debug(response.message);
+			if (response.message == 'found success') {
+				chrome.tabs.remove(tab.id, function (){});
+			} else {
+				console.debug('Cannot close yet, will try again in 5 seconds');
+				repeatedlyAttemptClose(tab);
+			}
+		});
+	}, 5000);
+}
+
+// Delete this. Neither case ever occurs.
+chrome.extension.onRequest.addListener(function(request, sender) {
 	if (request.message == "popup requested a close") {
-console.debug('delete this, it is never called');
+		console.debug('delete this, it is never called');
 		var tab = request.tab;	
 		
 		if (tab.url.startsWith('http://netgear')) {
@@ -55,19 +70,3 @@ console.debug('delete this, it is never called');
 	}
 });
 
-function repeatedlyAttemptClose(tab){
-	console.debug('repeatedlyAttemptClose() begin');
-	// Wait 5 seconds. It seems like sending it immediately doesn't give a chance for the content script to load
-	setTimeout(function () {
-		// Check if stash was successful
-		chrome.tabs.sendMessage(tab.id, {tab: tab.id, counter: 0, message : "was stashing tab " + tab.id +  " successful?" }, null, function handler(response) {
-			console.debug(response.message);
-			if (response.message == 'found success') {
-				chrome.tabs.remove(tab.id, function (){});
-			} else {
-				console.debug('Cannot close yet, will try again in 5 seconds');
-				repeatedlyAttemptClose(tab);
-			}
-		});
-	}, 5000);
-}
