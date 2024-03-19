ec4503fee0c1e4b0d9adcdd7107fd977ac5a2c34
Sridhar Sarnobat <ss401533@gmail.com>
2024-03-18 23:07:12 -0700

    works with v3 manifest

---
+++ close_yurled/service_worker.js
@@ -0,0 +1,38 @@
+// 0.1 takes too much processing. Every 30-60 secs would be better.
+chrome.alarms.create("Close unprivileged stashed windows", { 
+	delayInMinutes: 0.1,
+	periodInMinutes: 0.1
+});
+       
+chrome.alarms.onAlarm.addListener(function( alarm ) {
+
+	chrome.tabs.query({}, function(tabs) {
+
+//	chrome.tabs.getAllInWindow(null, function(tabs){
+		for (var i = 0; i < tabs.length; i++) {
+			//chrome.tabs.sendRequest(tabs[i].id, { action: "xxx" });
+			var tab = tabs[i];
+			var url = tabs[i].url;
+//			console.log("-");
+			if (url.includes('netgear.rohidekar.com/yurl/stash') ||
+			    url.includes('netgear.rohidekar.com/yurl/httpcat')) {
+				console.log("(sending message to stashed tab - but may not be successful yet): " + url);
+				chrome.tabs.sendMessage(tab.id, {tab: tab, counter: 0, message : "was stashing tab " + tab.id +  " successful?" }, null, function handler(response) {
+					// doesn't work
+					//debugger;
+					console.debug("chrome.runtime.onMessage = " + chrome.runtime.onMessage);
+					console.debug(response);
+					console.debug(response.message);
+					if (response.message == 'found success') {
+//						alert('closing:  ' + tab.url);
+						chrome.tabs.remove(response.tab.id, function (){});
+					} else {
+						console.debug('Cannot close yet, will try again later');
+					}
+				});
+			}
+		}
+	});
+});
+
+//chrome.alarms.create("Close unprivileged stashed windows", object alarmInfo);
